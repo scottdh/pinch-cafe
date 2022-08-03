5299d3707b4fe37ee6414c154cbb72034649064a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-08-03 12:13:36 +0100

    Update getData.py

--- src/TFL/getData.py
+++ src/TFL/getData.py
@@ -9,10 +9,10 @@ def readFile(theFilename):
   return result
 
 def printCommand(theCommand):
-  print(theCommand)
+  #print(theCommand)
   os.system(theCommand + " 2>&1")
 
 jsonData = json.loads(readFile("secrets.json"))
 printCommand("curl https://tfl.gov.uk/tfl/syndication/feeds/serviceboard-fullscreen.htm --output www/index.html")
 printCommand("curl https://api.tfl.gov.uk/Line/northern/Arrivals?app_id=" + jsonData["tfl"]["app_id"] + "\&app_key=" + jsonData["tfl"]["app_key"] + " --output www/northernArrivals.json")
-printCommand("curl https://api.tfl.gov.uk/Line/northern/Status?detail=true&app_id=" + jsonData["tfl"]["app_id"] + "\&app_key=" + jsonData["tfl"]["app_key"] + " --output www/northernStatus.json")
+printCommand("curl https://api.tfl.gov.uk/Line/northern/Status?detail=true\&app_id=" + jsonData["tfl"]["app_id"] + "\&app_key=" + jsonData["tfl"]["app_key"] + " --output www/northernStatus.json")
